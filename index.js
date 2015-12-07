6e8f2541593a493c07ac255b95ddf258fcbefad5
TheEarl <TheEarl1224@googlemail.com>
2015-12-07 13:30:56 +0100

    Localize

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.on('window-all-closed', function () {
 })
 
 app.on('ready', function () {
-  mainWindow = new BrowserWindow({width: 400, height: 500, icon: 'icon.ico'})
+  mainWindow = new BrowserWindow({width: 400, height: 500, icon: 'icon.ico', title: 'Shutdown Timer'})
   mainWindow.setResizable(false)
   mainWindow.setMenu(null)
   mainWindow.loadURL('file://' + __dirname + '/index.html')
